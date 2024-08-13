25a207b2075d1f3a9e684e48ab54ec68eb4172c1
ALEX PARSHIKOV <parahumanoid2@gmail.com>
2024-08-13 17:17:16 +0300

    notes6.commit1

--- js/task-1.js
+++ js/task-1.js
@@ -1,44 +1,47 @@
-const getUserNames = (users) => {
-  return users.map(user=>user.name);
-};
+class User {
+  email;
 
-console.log(
-  getUserNames([
-  {
-    name: "Moore Hensley",
-    email: "moorehensley@indexia.com",
-    balance: 2811
-  },
-  {
-    name: "Sharlene Bush",
-    email: "sharlenebush@tubesys.com",
-    balance: 3821
-  },
-  {
-    name: "Ross Vazquez",
-    email: "rossvazquez@xinware.com",
-    balance: 3793
-  },
-  {
-    name: "Elma Head",
-    email: "elmahead@omatom.com",
-    balance: 2278
-  },
-  {
-    name: "Carey Barr",
-    email: "careybarr@nurali.com",
-    balance: 3951
-  },
-  {
-    name: "Blackburn Dotson",
-    email: "blackburndotson@furnigeer.com",
-    balance: 1498
-  },
-  {
-    name: "Sheree Anthony",
-    email: "shereeanthony@kog.com",
-    balance: 2764
-  },
-])
-); // ["Moore Hensley", "Sharlene Bush", "Ross Vazquez", "Elma Head", "Carey Barr", "Blackburn Dotson", "Sheree Anthony"]
-console.log("-----------------END TASK 1-------------------")
+  constructor(email) {
+    this.email = email;
+  }
+
+  get email() {
+    return this.email;
+  }
+
+  set email(newEmail) {
+    this.email = newEmail;
+  }
+}
+//--------------------------
+class Admin extends User {
+  static role = {
+    BASIC: "basic",
+    SUPERUSER: "superuser",
+  };
+  static blacklistedEmails = [];
+  static blacklist(email){
+    if(!(Admin.blacklisedEmails.contains(email)))
+       Admin.blacklistedEmails.push(email);
+  }
+  static isBlacklisted(email){
+    return Admin.blacklistedEmails.contains(email);
+  }
+  constructor(params) {
+    super(params.email);
+    this.access = params.access;
+  }
+}
+//-------------------------
+const mango = new Admin({
+  email: "mango@mail.com",
+  access: Admin.role.SUPERUSER,
+});
+
+console.log(mango.email); // "mango@mail.com"
+console.log(mango.access); // "superuser"
+
+mango.blacklist("poly@mail.com");
+console.log(mango.blacklistedEmails); // ["poly@mail.com"]
+console.log(mango.isBlacklisted("mango@mail.com")); // false
+console.log(mango.isBlacklisted("poly@mail.com")); // true
