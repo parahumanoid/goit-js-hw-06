c7f6bf6c091a95f0c38e467ad5d7803e40384360
ALEX PARSHIKOV <parahumanoid2@gmail.com>
2024-08-13 18:16:28 +0300

    task1.commit

--- js/task-1.js
+++ js/task-1.js
@@ -1,51 +1,31 @@
-class User {
-  email;
+const customer = {
+  username: "Mango",
+  balance: 24000,
+  discount: 0.1,
+  orders: ["Burger", "Pizza", "Salad"],
+  // Change code below this line
+  getBalance() {
+    return this.balance;
+  },
+  getDiscount() {
+    return this.discount;
+  },
+  setDiscount(value) {
+    this.discount = value;
+  },
+  getOrders() {
+    return this.orders;
+  },
+  addOrder(cost, order) {
+    this.balance -= cost - cost * this.discount;
+    this.orders.push(order);
+  },
+  // Change code above this line
+};
 
-  constructor(email) {
-    this.email = email;
-  }
-
-  get email() {
-    return this.email;
-  }
-
-  set email(newEmail) {
-    this.email = newEmail;
-  }
-}
-class Admin extends User {
-  static role = {
-    BASIC: "basic",
-    SUPERUSER: "superuser",
-  };
-
-  blacklistedEmails = [];
-
-  constructor(params) {
-    super(params.email);
-    this.access = params.access;
-  }
-
-  blacklist(email) {
-    if (this.blacklistedEmails.includes(email)===false) {
-      this.blacklistedEmails.push(email);
-    }
-  }
-
-  isBlacklisted(email) {
-    return this.blacklistedEmails.includes(email);
-  }
-}
-
-const mango = new Admin({
-  email: "mango@mail.com",
-  access: Admin.role.SUPERUSER,
-});
-
-console.log(mango.email); // "mango@mail.com"
-console.log(mango.access); // "superuser"
-
-mango.blacklist("poly@mail.com");
-console.log(mango.blacklistedEmails); // ["poly@mail.com"]
-console.log(mango.isBlacklisted("mango@mail.com")); // false
-console.log(mango.isBlacklisted("poly@mail.com")); // true
+customer.setDiscount(0.15);
+console.log(customer.getDiscount()); // 0.15
+customer.addOrder(5000, "Steak");
+console.log(customer.getBalance()); // 19750
+console.log(customer.getOrders()); // ["Burger", "Pizza", "Salad", "Steak"]
+-------------------END TASK 1--------------------
